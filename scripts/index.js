e4e5f9e092c46ded815d6f534349f701318393f8
Kausik Bhaumik <kausikbhaumik42@gmail.com>
2022-07-23 10:58:03 +0530

    new

--- scripts/index.js
+++
@@ -1,7 +0,0 @@
-import products from "../db/db.js";
-console.log('products:', products)
-let displayData = () => {
-    let divMain = document.createElement("div");
-    let divM1 = document.createElement("div");
-    let img = document.createElement("img");
-}
